8948829b9e485b77d1c42ff427df620b75bfb6e2
Dheison Gomes <dheisomgomes0@gmail.com>
2024-02-01 08:54:07 -0300

    Updated descriptions, removed Instagram link and added Reddit profile, use more semantic HTML too

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const projectsContainer = document.querySelector("div.project-list")
-const contactsContainer = document.querySelector("div.contact")
-
 const project = (title, description, url) => ({
   title, description, url,
-  render: function () {
+  render: function() {
     return `
+    <li>
       <a class="project" href="${this.url}">
         <h3>${this.title}</h3>
         <span>${this.description}</span>
       </a>
-    `
+    </li>`
   }
 })
 
@@ -17,17 +15,18 @@ const link = (title, url, icon) => ({
   title, url, icon,
   render: function () {
     return `
+    <li>
       <a href="${this.url}" alt="${this.title}">
         <ion-icon name="${this.icon}" aria-hidden="true"></ion-icon>
       </a>
-    `
+    </li>`
   }
 })
 
 const links = [
-  link("Github", "https://github.com/dheison0", "logo-github"),
-  link("Instagram", "https://instagram.com/dheison0", "logo-instagram"),
+  link("Reddit", "https://www.reddit.com/user/dheison0", "logo-reddit"),
   link("Telegram", "https://t.me/dheison0", "paper-plane-outline"),
+  link("Github", "https://github.com/dheison0", "logo-github"),
   link("Tabnews", "https://www.tabnews.com.br/dheison0", "folder-outline"),
   link("E-mail", "mailto:dheisomgomes0@gmail.com", "mail-outline"),
 ]
@@ -35,17 +34,17 @@ const links = [
 const projects = [
   project(
     "Anna's Archive",
-    "Aplicativo não oficial criado para android usando React Native com a finalidade de facilitar o download de livros da biblioteca Anna's Archive.",
+    "App não oficial do website Annas Archive criado usando React Native, possui algumas funcionalidades úteis.",
     "https://github.com/dheison0/annas-archive"
   ),
   project(
     "Anna's Archive API",
-    "API não oficial do website Anna's Archive criado usando Python & BeautifulSoup4 utilizando raspagem de dados,",
+    "API não oficial do website Anna's Archive criado usando Python & BeautifulSoup4 para raspagem de dados.",
     "https://github.com/dheison0/annas-archive-api"
   ),
   project(
     "Telegram Downloader",
-    "Bot para telegram com o intuito de facilitar o download de conteúdos públicados em canais por lá fazendo-os diretamente em seu servidor ou NAS.",
+    "Bot para telegram útil para automatizar o download de arquivos enviados nos canais em que está inscrito.",
     "https://github.com/dheison0/telegram-downloader"
   ),
   project(
@@ -55,10 +54,11 @@ const projects = [
   ),
 ]
 
+const renderAll = items => items.map(i => i.render()).join('\n')
+
 function onLoad() {
-  const renderAll = a => a.map(i => i.render()).join('\n')
-  projectsContainer.innerHTML = renderAll(projects)
-  contactsContainer.innerHTML = renderAll(links)
+  document.querySelector("ul.project-list").innerHTML = renderAll(projects)
+  document.querySelector("ul.contact").innerHTML = renderAll(links)
 }
 
 onLoad()
